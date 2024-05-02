3a5db9b89c6167dba4af07e8f2c1885ca68b74a6
Mintype <900raiyan@gmail.com>
2024-05-02 17:40:45 -0400

    Added routes for pages.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 import Home from "./pages/Home";
 import New from "./pages/New";
 import AppWrapper from "./components/AppWrapper";
+import TermsOfService from "./pages/TermsOfService";
+import Search from "./pages/Search";
+import Privacy from "./pages/Privacy";
+import MySets from "./pages/MySets";
+import Me from "./pages/Me";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -12,6 +19,13 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/me" element={<Me />} />
+          <Route path="/mysets" element={<MySets />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/terms" element={<TermsOfService />} />
         </Routes>
       </Router>
     </AppWrapper>
