33c2f7c4ba6c6d65e4e540c6c18e600a0c12ee95
Mintype <900raiyan@gmail.com>
2024-05-07 18:35:09 -0400

    add redirect for button

--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -7,7 +7,7 @@ function FrontPage() {
       <div className='top-container'>
         <h1 className='top-text'>Tired of long late nights studying for tests?</h1>
         <p>LernIt is an open source learning platform that is committed to making education more accessible than ever before! Our mission is to empower learners of all ages and backgrounds by offering a wide range of customizable solutions and cutting-edge features tailored to enhance the learning experience. </p>
-        <button className='btn-signup'>Sign Up</button>
+        <a href="/signin"><button className='btn-signup'>Sign Up</button> </a>
       </div>
     </div>
   )
