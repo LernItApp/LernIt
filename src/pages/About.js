ef9ec3cec3262500b18302798e05b481df89e9b9
Mintype <900raiyan@gmail.com>
2024-05-09 16:39:21 -0400

    add code to about js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import '../styles/about.css'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='container'>
+      <div className='top-container'>
+        <h1 className='top-text'>About Us</h1>
+        <p>LernIt is an open source learning platform that is committed to making education more accessible than ever before! Our mission is to empower learners of all ages and backgrounds by offering a wide range of customizable solutions and cutting-edge features tailored to enhance the learning experience. </p>
+        
+        <h1 className='mid-text'>About Mintype</h1>
+        <p>I'm a highschool student who is obsessed with everything computer science. After initially learning Java in school, which opened my eyes to the endless possibilities that come with coding, I went deeper into various branches of computer science. I explored game development, web development, app development, and more. Now I also have a programming YouTube channel, where I teach people how to code!</p>
+      </div>
+  </div>
   )
 }
 
