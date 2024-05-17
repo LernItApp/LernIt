813000d59ff59bde10e0295cd1042bb239964e82
Mintype <900raiyan@gmail.com>
2024-05-17 16:56:02 -0400

    enhance search bar

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import { React, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 
 function Search() {
   const { id } = useParams();
+
+  useEffect(() => {
+    const searchBar = document.querySelector('.searchBar');
+    if (searchBar) {
+      searchBar.value = id;
+    }
+  }, [id]);
 
   return (
     <div>
