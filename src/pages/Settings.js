22e03d47ef971b93856fda32c8c1211e5088c63f
Mintype <900raiyan@gmail.com>
2024-05-19 11:35:37 -0400

    Got rid of toggles

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,5 +1,5 @@
 import { React, useEffect, useState } from 'react'
-import ToggleSwitch from '../components/ToggleSwitch';
+//import ToggleSwitch from '../components/ToggleSwitch';
 
 function Settings() {
   const [name, setName] = useState('');
@@ -44,11 +44,20 @@ function Settings() {
         <label> Username <input name="Username" value={username} onChange={handleUsernameChange} /> </label>
         <label> Email <input name="Email" value={email} onChange={handleEmailChange} /> </label>
         <hr />
+        <label> Dark Mode <input name="DarkMode" type="checkbox" checked={darkmode} onChange={handleDarkModeToggle} /> </label>
+        <label> Sound Effects <input name="SoundEffects" type="checkbox" checked={soundeffects} onChange={handleSoundEffectsToggle} /> </label>
+        <label> Notifications <input name="Notifications" type="checkbox" checked={notifications} onChange={handleNotificationsToggle} /> </label>
+
+
+
+
+
+
         {/* make the below work with the usestate! */}
-        <ToggleSwitch label="Dark Mode" isChecked={darkmode} onChange={handleDarkModeToggle} />
+        {/* <ToggleSwitch label="Dark Mode" isChecked={darkmode} onChange={handleDarkModeToggle} />
         <ToggleSwitch label="Sound Effects" isChecked={soundeffects} onChange={handleSoundEffectsToggle} />
         <ToggleSwitch label="Notifications" isChecked={notifications} onChange={handleNotificationsToggle} />
- 
+  */}
       </div>
 
     </div>
