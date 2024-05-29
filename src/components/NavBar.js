964c3fcedbc81134caf5139430c69701245d89bc
Mintype <96578565+Mintype@users.noreply.github.com>
2024-05-29 17:42:52 -0400

    fix navbar not updating on auth bug

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/NavBar.css';
 import Cookies from "universal-cookie";
 import { Auth } from "../components/Auth.js";
-
+import { getAuth, onAuthStateChanged } from "firebase/auth"; // Import Firebase Auth methods
 const cookies = new Cookies();
 
 function NavBar() {
     
     const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
-
     const [inputValue, setInputValue] = useState('');
+
+  // Set up Firebase authentication state listener
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuth(true);
+        cookies.set("auth-token", user.refreshToken);
+      } else {
+        setIsAuth(false);
+        cookies.remove("auth-token");
+      }
+    });
+
+    // Clean up the subscription on unmount
+    return () => unsubscribe();
+  }, []);
 
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
