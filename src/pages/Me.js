d9a909e91ad232b9b183e3c82ab0ecf4b3b68b29
Mintype <96578565+Mintype@users.noreply.github.com>
2024-06-25 21:32:29 -0400

    Worked on /me page

--- src/pages/Me.js
+++ src/pages/Me.js
@@ -44,6 +44,7 @@ function Me() {
   }, []);
 
   const [myStudyLists, setMyStudyLists] = useState(null);
+  const [myTrendingStudyLists, setMyTrendingStudyLists] = useState(null);
 
   useEffect(() => {
     const fetchStudyGuides = async () => {
@@ -99,33 +100,111 @@ function Me() {
 
   }, []);
 
+  useEffect(() => {
+    const fetchTrendingStudyGuides = async () => {
+      try {
+          const q = query(
+            collection(db, 'studylists'),
+            orderBy('timesStudied', 'desc'),
+            limit(10)
+          );
+
+          const querySnapshot = await getDocs(q);
+
+          // const querySnapshot = await getDocs(collection(db, 'studylists'));
+
+          const studyGuidesArray = [];
+
+          querySnapshot.forEach((doc) => {
+              if (doc.exists()) {
+                  const { title, user, userId, termCount, timesStudied } = doc.data();
+                  const studyGuide = {
+                      id: doc.id,
+                      userid: userId,
+                      user: user,
+                      title: title,
+                      termCount: termCount,
+                      timesStudied: timesStudied
+                  };
+
+                  // for testing purposes.
+                  console.log("guide id: " + studyGuide["userid"]);
+                  // console.log("user id: " + auth.currentUser.uid);
+                  console.log("user id: " + auth.currentUser.uid + "\n");
+                  console.log("term count: " + studyGuide["termCount"] + "\n");
+                  
+                  // only adds studylist if user made it.
+                  if(studyGuide["userid"] == auth.currentUser.uid)
+                    studyGuidesArray.push(studyGuide);
+
+                  setMyTrendingStudyLists(studyGuidesArray);
+              } else {
+                  console.log("Document does not exist:", doc.id);
+              }
+          });
+
+          console.log("Study Guides Array:", studyGuidesArray);
+          return studyGuidesArray;
+      } catch (error) {
+          console.error("Error fetching study guides:", error);
+          // Handle error fetching study guides
+          return [];
+      }
+    };
+
+    fetchTrendingStudyGuides();
+
+  }, []);
+
   if (loading) {
     return <div>Loading...</div>;
   }
 
   return (
     <div className='pageContainer'>
-      <div className='user-container'>
-        {photoURL && <img className='profileImage' src={photoURL} alt="Profile" />}
-        <h1 className='username'>{displayName}</h1>
-      </div>
-      <div className='list-container'>
-            <h2 className='list-container-title'>Your Study Lists</h2>
-            <div className='list-item-container'>
-              {myStudyLists ? (
-                myStudyLists.map((guide) => (
-                  <a className='box-a' href={`/list/${guide.id}`} key={guide.id}>
-                    <div className='exampleBox'>
-                      <p className='exampleBoxText'>{guide.title}</p>
-                      <p className='exampleBoxText'>{guide.termCount}</p>
-                    </div> 
-                  </a>
-                ))
-              ) : (
-                <div className='exampleBox'>Study list loading... 😔</div>
-              )}
-            </div>
+        <div className='user-container'>
+          {photoURL && <img className='profileImage' src={photoURL} alt="Profile" />}
+          <h1 className='username'>{displayName}</h1>
+        </div>
+
+        <div className='list-container'>
+          <h2 className='list-container-title'>Your Study Lists</h2>
+          <div className='list-item-container'>
+            {myStudyLists ? (
+              myStudyLists.map((guide) => (
+                <a className='box-a' href={`/list/${guide.id}`} key={guide.id}>
+                  <div className='exampleBox'>
+                    <p className='exampleBoxText'>{guide.title}</p>
+                    <p className='exampleBoxText'>{guide.termCount}</p>
+                  </div> 
+                </a>
+              ))
+            ) : (
+              <div className='exampleBox'>Study list loading... 😔</div>
+            )}
           </div>
+      </div>
+
+      <div className='list-container' id="watewfwef">
+          <h2 className='list-container-title'>Your Trending Study Lists</h2>
+          <div className='list-item-container'>
+            {myTrendingStudyLists ? (
+              myTrendingStudyLists.map((guide) => (
+                <a className='box-a' href={`/list/${guide.id}`} key={guide.id}>
+                  <div className='exampleBox'>
+                    <p className='exampleBoxText'>{guide.title}</p>
+                    <p className='exampleBoxText'>{guide.termCount}</p>
+                    <p className='exampleBoxText'>Times Studied: {guide.timesStudied}</p>
+                  </div> 
+                </a>
+              ))
+            ) : (
+              <div className='exampleBox'>Study list loading... 😔</div>
+            )}
+          </div>
+      </div>
+
+
     </div>
   );
 }
