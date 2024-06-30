c5ade8004e7fcd1463f69bdc20780f8391bcc99e
Mintype <96578565+Mintype@users.noreply.github.com>
2024-06-30 19:12:13 -0400

    working on flashcards

--- src/pages/TermsOfService.js
+++ src/pages/TermsOfService.js
@@ -1,8 +1,58 @@
 import React from 'react'
+import '../styles/TermsOfService.css'
+
 
 function TermsOfService() {
   return (
-    <div>TermsOfService</div>
+    <div className='termsofservicecontainer'>
+
+      <h1>Terms of Service</h1>
+      <h4>Last updated June 23, 2024</h4>
+      <h2>AGREEMENT TO OUR LEGAL TERMS</h2>
+      <p>We are LernIt ("Company," "we," "us," "our").</p>
+      <p>We operate the website lernit-78956.web.app (the "Site"), the mobile application LernIt (the "App"), as well as any other related products and services that refer or link to these legal terms (the "Legal Terms") (collectively, the "Services").</p>
+      <p>LernIt is a web based learning/studying platform dedicated to making learning faster and easier than never before!</p>
+      <p>These Legal Terms constitute a legally binding agreement made between you, whether personally or on behalf of an entity ("you"), and LernIt, concerning your access to and use of the Services. You agree that by accessing the Services, you have read, understood, and agreed to be bound by all of these Legal Terms. IF YOU DO NOT AGREE WITH ALL OF THESE LEGAL TERMS, THEN YOU ARE EXPRESSLY PROHIBITED FROM USING THE SERVICES AND YOU MUST DISCONTINUE USE IMMEDIATELY.</p>
+      <p>Supplemental terms and conditions or documents that may be posted on the Services from time to time are hereby expressly incorporated herein by reference. We reserve the right, in our sole discretion, to make changes or modifications to these Legal Terms from time to time. We will alert you about any changes by updating the "Last updated" date of these Legal Terms, and you waive any right to receive specific notice of each such change. It is your responsibility to periodically review these Legal Terms to stay informed of updates. You will be subject to, and will be deemed to have been made aware of and to have accepted, the changes in any revised Legal Terms by your continued use of the Services after the date such revised Legal Terms are posted.</p>
+      <p>All users who are minors in the jurisdiction in which they reside (generally under the age of 18) must have the permission of, and be directly supervised by, their parent or guardian to use the Services. If you are a minor, you must have your parent or guardian read and agree to these Legal Terms prior to you using the Services.</p>
+
+      <p>We recommend that you print a copy of these Legal Terms for your records.</p>
+
+      <h2>TABLE OF CONTENTS</h2>
+      <ol type="1">
+        <li className='tableofcontentsatags'><a className='tableofcontentsatags' href='#ourservices'>OUR SERVICES</a></li>
+        <li className='tableofcontentsatags'><a href='#intellectualpropertyrights'>INTELLECTUAL PROPERTY RIGHTS</a></li>
+        <li className='tableofcontentsatags'><a href='#'></a></li>
+        <li className='tableofcontentsatags'><a href='#'></a></li>
+        <li className='tableofcontentsatags'><a href='#'></a></li>
+        <li className='tableofcontentsatags'><a href='#'></a></li>
+        <li className='tableofcontentsatags'><a href='#'></a></li>
+      </ol>
+
+      <h2 id='ourservices'>1. OUR SERVICES</h2>
+      <p>The information provided when using the Services is not intended for distribution to or use by any person or entity in any jurisdiction or country where such distribution or use would be contrary to law or regulation or which would subject us to any registration requirement within such jurisdiction or country. Accordingly, those persons who choose to access the Services from other locations do so on their own initiative and are solely responsible for compliance with local laws, if and to the extent local laws are applicable.</p>
+      <p>The Services are not tailored to comply with industry-specific regulations (Health Insurance Portability and Accountability Act (HIPAA), Federal Information Security Management Act (FISMA), etc.), so if your interactions would be subjected to such laws, you may not use the Services. You may not use the Services in a way that would violate the Gramm-Leach-Bliley Act (GLBA).</p>
+      
+      <h2 id='intellectualpropertyrights'>2. INTELLECTUAL PROPERTY RIGHTS</h2>
+      <h3>Our intellectual property</h3>
+      <p>We are the owner or the licensee of all intellectual property rights in our Services, including all source code, databases, functionality, software, website designs, audio, video, text, photographs, and graphics in the Services (collectively, the "Content"), as well as the trademarks, service marks, and logos contained therein (the "Marks").</p>
+      <p>Our Content and Marks are protected by copyright and trademark laws (and various other intellectual property rights and unfair competition laws) and treaties in the United States and around the world.</p>
+      <p>The Content and Marks are provided in or through the Services "AS IS" for your personal, non-commercial use only.</p>
+      <h3>Your use of our Services</h3>
+      <p>Subject to your compliance with these Legal Terms, including the "PROHIBITED ACTIVITIES" section below, we grant you a non-exclusive, non-transferable, revocable license to:</p>
+      <ul>
+        <li className='tableofcontentsatags'>access the Services; and</li>
+        <li className='tableofcontentsatags'>download or print a copy of any portion of the Content to which you have properly gained access.</li>
+      </ul>
+      <p>solely for your personal, non-commercial use.</p>
+      <p>Except as set out in this section or elsewhere in our Legal Terms, no part of the Services and no Content or Marks may be copied, reproduced, aggregated, republished, uploaded, posted, publicly displayed, encoded, translated, transmitted, distributed, sold, licensed, or otherwise exploited for any commercial purpose whatsoever, without our express prior written permission.</p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      
+    </div>
   )
 }
 
