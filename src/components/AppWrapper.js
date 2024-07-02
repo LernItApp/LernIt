1094d04c3306467b7cfca8c921a7aa3a97a6e1ce
Mintype <96578565+Mintype@users.noreply.github.com>
2024-07-02 16:38:39 -0400

    added github icon in corner yay!

--- src/components/AppWrapper.js
+++ src/components/AppWrapper.js
@@ -6,6 +6,7 @@ import '../styles/ScrollBar.css';
 import { signOut } from "firebase/auth";
 import Cookies from "universal-cookie";
 import { auth } from "../firebase-config.js";
+import logo from './githubicon.webp';
 
 const cookies = new Cookies();
 
@@ -22,6 +23,10 @@ function AppWrapper({ children }) {
     <div className="app-container">{children}</div>
 
     <Footer />
+
+    <a class="github-icon" href="https://github.com/LernItApp/LernIt" target="_blank">
+            <img src={logo} alt="GitHub Repository"/>
+    </a>
 
     </div>
   )
